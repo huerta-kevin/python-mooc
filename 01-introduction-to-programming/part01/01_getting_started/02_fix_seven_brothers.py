a52ca173dfadea903530ce6b1f29c57622372960
huerta-kevin <huerta.s.kevin@icloud.com>
2025-06-09 18:47:43 -0700

    Complete section 1 exercises

---
+++ 01-introduction-to-programming/part01/01_getting_started/02_fix_seven_brothers.py
@@ -0,0 +1,10 @@
+# Please fix the program so that the names are printed in the correct order.
+
+# Fix the code
+print("Aapo")
+print("Eero")
+print("Juhani")
+print("Lauri")
+print("Simeoni")
+print("Timo")
+print("Tuomas")
---
+++ 01-introduction-to-programming/part01/01_getting_started/05_print_some_code.py
@@ -0,0 +1,5 @@
+# Please write a program which prints out the following:
+# print("Hello there!")
+
+# Write your solution here
+print('print("Hello there!")')
