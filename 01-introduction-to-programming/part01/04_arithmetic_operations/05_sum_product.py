93c479e5eb20ebdd6ed0be3fff86179bab6dc96b
huerta-kevin <huerta.s.kevin@icloud.com>
2025-06-19 19:33:18 -0700

    Complete section 4 exercises

---
+++ 01-introduction-to-programming/part01/04_arithmetic_operations/05_sum_product.py
@@ -0,0 +1,16 @@
+# Please write a program which asks the user for two numbers. The program will
+# then print out the sum and the product of the two numbers.
+
+# The program should function as follows:
+
+# Number 1: 3
+# Number 2: 7
+# The sum of the numbers: 10
+# The product of the numbers: 21
+
+# Write your solution here
+number1 = int(input("Number 1: "))
+number2 = int(input("Number 2: "))
+
+print("The sum of the numbers:", number1 + number2)
+print("The product of the numbers:", number1 * number2)
